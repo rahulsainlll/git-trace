ff8cd0269ddc53a3aee6c2708edd36e9c59b46d6
rahulsainlll <rahulsain.social777@gmail.com>
2024-08-05 23:10:40 +0530

    caching

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("yoo")}>Add to cart</button>
+    </div>
+  )
+}
+
+export default ProductCard
---
+++ app/users/new/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewUserPage = () => {
+  return (
+    <div>
+      NewUserPage
+    </div>
+  )
+}
+
+export default NewUserPage
