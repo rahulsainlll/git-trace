2e4b5410b2bc2d944bf125d4af736f04b147273c
rahulsainlll <rahulsain.social777@gmail.com>
2024-08-06 01:21:06 +0530

    active link

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,25 +1,34 @@
+"use client";
+
 import React from "react";
 import Link from "next/link";
-import { GoGitMergeQueue } from "react-icons/go";
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
-    { label: "Dashboard", href: "/dashboard" },
-    { label: "Issue", href: "/issue" },
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
   ];
 
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center font-mono">
-      <Link href="/">
-        <GoGitMergeQueue />
+      <Link href="/" className="text-lg">
+        Trace ↯
       </Link>
 
       <ul className="flex space-x-6">
         {links.map((link) => (
-          <Link 
-          href={link.href} 
-          key={link.href}
-          className="text-zinc-500 hover:text-zinc-800"
+          <Link
+            href={link.href}
+            key={link.href}
+            className={classNames({
+              "text-zinc-900": currentPath === link.href,
+              "text-zinc-500": currentPath !== link.href,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
           >
             {link.label}
           </Link>
---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+    IssuePage
+    </div>
+  )
+}
+
+export default IssuesPage;
