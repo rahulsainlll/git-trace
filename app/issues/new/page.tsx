fe0167de3d741c254196ed31b253aa04022ca521
rahulsainlll <rahulsain.social777@gmail.com>
2024-08-07 13:15:27 +0530

    submit

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -29,6 +29,17 @@ const NewIssuePage = () => {
     resolver: zodResolver(issueSchema),
   });
 
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setSubmitting(false);
+      setError("An unexpected error occurred.");
+    }
+  });
+
   return (
     <div className="max-w-2xl">
       {error && (
@@ -40,19 +51,7 @@ const NewIssuePage = () => {
         </Callout.Root>
       )}
 
-      <form
-        className=" space-y-3"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            setSubmitting(true);
-            await axios.post("/api/issues", data);
-            router.push("/issues");
-          } catch (error) {
-            setSubmitting(false);
-            setError("An unexpected error occurred.");
-          }
-        })}
-      >
+      <form className=" space-y-3" onSubmit={onSubmit}>
         <TextField.Root size="3" placeholder="Title" {...register("title")} />
         <ErrorMessage>{errors.title?.message}</ErrorMessage>
 
