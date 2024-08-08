33ed30d696dabd2579fb286208ee1db617870c23
rahulsainlll <rahulsain.social777@gmail.com>
2024-08-08 14:32:19 +0530

    pm

--- prisma/migrations/20240808084841_create_issue/migration.sql
+++ prisma/migrations/20240808084841_create_issue/migration.sql
@@ -5,7 +5,7 @@ CREATE TYPE "Status" AS ENUM ('OPEN', 'IN_PROGRESS', 'CLOSED');
 CREATE TABLE "Issue" (
     "id" SERIAL NOT NULL,
     "title" VARCHAR(255) NOT NULL,
-    "discription" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
     "status" "Status" NOT NULL DEFAULT 'OPEN',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updateAt" TIMESTAMP(3) NOT NULL,
