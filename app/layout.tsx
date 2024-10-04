487c1f51dd317ae6e81fa3d71c46f5d81adb4b9f
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-04 09:39:11 +0530

    added:: Meta Tags, Robots.txt, and Sitemap for Improved SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,45 @@ import { Footer } from "@/components/ui/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// fallback to the production URL
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "https://git-trace.vercel.app/";
+
 export const metadata: Metadata = {
   title: {
     default: "Git-Trace",
-    template: "%s | Git-Trace"
+    template: "%s | Git-Trace",
   },
   description:
     "Instantly search, view, and bookmark GitHub repositories and issues. Streamline your workflow and keep your favorite projects at your fingertips!",
+  openGraph: {
+    title: "Git-Trace",
+    description:
+      "Instantly search, view, and bookmark GitHub repositories and issues. Streamline your workflow and keep your favorite projects at your fingertips!",
+    type: "website",
+    url: baseUrl,
+    images: [
+      {
+        url: `${baseUrl}/git3.png`,
+        width: 800,
+        height: 600,
+        alt: "Git-Trace OG Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Git-Trace",
+    description:
+      "Instantly search, view, and bookmark GitHub repositories and issues.",
+    images: `${baseUrl}/git3.png`,
+  },
+  keywords: [
+    "GitHub repositories",
+    "GitHub issues",
+    "repository search",
+    "workflow tools",
+    "developer tools",
+  ],
 };
 
 export default function RootLayout({
