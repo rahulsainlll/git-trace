c62c767cd25f907fdc3d8f528ae3ad5d99aef2fd
Aditya Pandey <adityapandey2395@gmail.com>
2024-10-07 12:10:29 +0530

    Changed the img to Image in Blog card component

--- components/ui/blog-card.tsx
+++ components/ui/blog-card.tsx
@@ -1,10 +1,12 @@
-
-export const BlogCard = ({ title, id } : any) => {
+import Image from "next/image";
+export const BlogCard = ({ title, id }: any) => {
   return (
     <div className="bg-white border border-gray-200 rounded-lg overflow-hidden shadow-sm">
-      <img
+      <Image
         src="https://via.placeholder.com/300"
         alt="Post Thumbnail"
+        width={300}
+        height={300}
         className="w-full h-48 object-cover"
       />
       <div className="p-6">
